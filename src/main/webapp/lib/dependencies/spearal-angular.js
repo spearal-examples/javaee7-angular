936ffcd889bdc53b34851a055dd2e47963108af4
fwolff <fwolff@graniteds.org>
2014-09-12 15:43:41 +0200

    Simplify Spearal / Angular helper.

--- src/main/webapp/lib/dependencies/spearal-angular.js
+++ src/main/webapp/lib/dependencies/spearal-angular.js
@@ -1,41 +1,28 @@
-function spearalAngularConfigureResources($resourceProvider, spearalFactory) {
-
-	console.log("[Spearal] Configuring Angular resource provider");
-	
-	if (!spearalFactory)
-		spearalFactory = new SpearalFactory();
-	
-	function encode(data, headersGetter) {
-		var array = null;
-		if (data) {
-			console.log("[Spearal] Encoding data: " + data);
-			var encoder = spearalFactory.newEncoder();
-			encoder.writeAny(data);
-			array = new Uint8Array(encoder.buffer);
-		}
-		return array;
-	}
-
-	function decode(arraybuffer, headersGetter) {
-		var result = null;
-		if (arraybuffer && arraybuffer.byteLength > 0) {
-			console.log("[Spearal] Decoding array buffer: " + arraybuffer.byteLength + " bytes");
-			var decoder = spearalFactory.newDecoder(arraybuffer);
-			result = decoder.readAny();
-		}
-		return result;
-	}
+function spearalAngularConfigureResources(resourceProvider, spearalFactory) {
 	
 	var headers = {
 		"Accept": "application/spearal",
 		"Content-Type": "application/spearal"
 	};
+	
+	function encode(data, headersGetter) {
+		if (data) {
+			var encoder = spearalFactory.newEncoder();
+			encoder.writeAny(data);
+			return new Uint8Array(encoder.buffer);
+		}
+	}
 
-	var actions = $resourceProvider.defaults.actions;
+	function decode(arraybuffer, headersGetter) {
+		if (arraybuffer && arraybuffer.byteLength > 0) {
+			var decoder = spearalFactory.newDecoder(arraybuffer);
+			return decoder.readAny();
+		}
+	}
 
+	var actions = resourceProvider.defaults.actions;
 	for (method in actions) {
 		var action = actions[method];
-		
 		action.responseType = 'arraybuffer';
 		action.headers = headers;
 		action.isArray = false;
