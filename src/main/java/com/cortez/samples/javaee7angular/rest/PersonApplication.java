7ef41426d6e6683026aa3851c5115ab1665f175f
fwolff <fwolff@graniteds.org>
2014-09-12 15:14:09 +0200

    Use a more explicit way of enabling Spearal.

--- src/main/java/com/cortez/samples/javaee7angular/rest/PersonApplication.java
+++ src/main/java/com/cortez/samples/javaee7angular/rest/PersonApplication.java
@@ -1,27 +1,40 @@
 package com.cortez.samples.javaee7angular.rest;
 
-import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import org.spearal.DefaultSpearalFactory;
 import org.spearal.SpearalFactory;
+import org.spearal.jpa2.EntityManagerWrapper;
 import org.spearal.jpa2.SpearalConfigurator;
 
 @ApplicationPath("/resources")
 public class PersonApplication extends Application {
-	
-	@PersistenceUnit
-	private EntityManagerFactory entityManagerFactory;
-	
-	@Produces
-	private SpearalFactory spearalFactory = new DefaultSpearalFactory();
-	
-	@PostConstruct
-	public void init() {
-		SpearalConfigurator.init(spearalFactory, entityManagerFactory);
-	}
+
+	@Produces @PersistenceContext
+	private EntityManager entityManager;
+
+//	@PersistenceContext
+//	private EntityManager entityManager;
+//	
+//	@Produces
+//	public EntityManager getEntityManager() {
+//		return new EntityManagerWrapper(entityManager);
+//	}
+//	
+//	@PersistenceUnit
+//	private EntityManagerFactory entityManagerFactory;
+//	
+//	@Produces @ApplicationScoped
+//	public SpearalFactory getSpearalFactory() {
+//		SpearalFactory spearalFactory = new DefaultSpearalFactory();
+//		SpearalConfigurator.init(spearalFactory, entityManagerFactory);
+//		return spearalFactory;
+//	}
 }
